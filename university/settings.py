5bff453d130c40e5760f693e2ecd07be90a2ed8e
AntiVassal <anti.vassal@gmail.com>
2026-05-19 10:40:02 +0300

    Add PostgreSQL support and Docker Compose setup
    Updated the `Dockerfile` to install `libpq5` for PostgreSQL.
    Configured `settings.py` to switch between PostgreSQL and SQLite
    based on environment variables. Added `compose.yaml` to define
    Docker Compose services for the database and Django app.
    
    Updated `requirements.txt` to include JWT authentication support.
    Introduced a `pgdata` volume for persistent PostgreSQL data storage.

--- university/settings.py
+++ university/settings.py
@@ -127,12 +127,24 @@ WSGI_APPLICATION = 'university.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('POSTGRES_HOST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB', 'university'),
+            'USER': os.environ.get('POSTGRES_USER', 'university'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'university'),
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
