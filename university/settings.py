90f9b710f6ea068f9a30afe9eea2566e1f2fb77a
AntiVassal <anti.vassal@gmail.com>
2026-05-19 06:29:07 +0300

    Update dependencies and enhance REST framework settings
    - Updated `requirements.txt` with new and updated dependencies.
    - Added `django_filters` and `drf_spectacular` to `INSTALLED_APPS`.
    - Configured `DEFAULT_FILTER_BACKENDS` for filtering, searching, and ordering.
    - Increased `PAGE_SIZE` from 10 to 20.
    - Set `DEFAULT_SCHEMA_CLASS` to `drf_spectacular.openapi.AutoSchema`.

--- university/settings.py
+++ university/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',
 
     'academics',
     'students',
@@ -57,8 +59,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
